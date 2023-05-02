7580718b47c76330688a3fa25e36fab328ba17f0
minashaker77 <minashk1998@gmail.com>
2023-05-02 18:30:25 +0430

    not success in workout

---
+++ workout-fifo/node.js
@@ -0,0 +1,10 @@
+
+var testArray = [1,2,3,4,577,67,'a','v']
+
+var push = function(testArray){
+    return testArray + 'Mina'
+}
+
+var pull = function(testArray){
+    return console.log(testArray) & testArray.length -1
+}

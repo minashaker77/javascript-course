ec8c60f9958d0deaf47985eac31eaadcb6da250f
minashaker77 <minashk1998@gmail.com>
2023-05-04 20:08:40 +0430

    use window location for navigation

---
+++ 2/7/script.js
@@ -0,0 +1,28 @@
+
+var submitForm = function (event) {
+    if(event) event.preventDefault()
+    if(document.getElementsByName('username')[0] && 
+    document.getElementsByName('password')[0]
+    ){
+        var username = document.getElementsByName('username')[0].value
+        var password = document.getElementsByName('password')[0].value
+        console.log('username' , username)
+        console.log('password' , password)
+        document.getElementById('descriptionP').innerHTML = 'Submitted'
+        // document.getElementById('descriptionP').innerHTML = '<h1>Submitted</h1>'
+        document.getElementById('descriptionP').style.color ="green"
+        document.getElementById('descriptionP').style.fontSize ="30px"
+        document.getElementById('descriptionP').style.border ="1px solid green"
+    } else{
+        document.getElementById('descriptionP').innerHTML = 'Error'
+    }
+}
+var onBirthdayChange = function (event) {
+    console.log('birthday:' , new Date(event.target.value))
+}
+var onLoadPageToUpdateInformation = function () {
+    document.getElementById('information').innerHTML = "<div>" + location.host + "</div>"
+}
+var goToGoogle = function () {
+    location.href = "https://www.google.com/"
+}

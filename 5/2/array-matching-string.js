e8e016c4be5345c93bee742f676eb025c0a29759
minashaker77 <minashk1998@gmail.com>
2023-05-06 18:54:17 +0430

    edit file's name and it's content

--- 5/2/array-matching-string.js
+++ 5/2/array-matching-string.js
@@ -3,9 +3,9 @@ const name = 'mina';
 //     age: 24,
 //     name: name,
 // }
-const person ={
-    age: 24,
-    name,
+const person = {
+        age: 24,
+        name,
 }
 //==> {age: 24, name: 'mina'}
 // const person1 = {
@@ -16,46 +16,46 @@ const person ={
 person1['education-1'] = 'bachelor'
 
 // ==> {age: 24, name: 'mina', education_1: 'bachelor', education-1: 'bachelor'}
-const person2 ={
-    age: 24,
-    name,
-    ['education-1'] : 'bachelor'
+const person2 = {
+        age: 24,
+        name,
+        ['education-1']: 'bachelor'
 }
 //{age: 24, name: 'mina', education-1: 'bachelor'}
 const number = 10;
-const person3 ={
-    age: 24,
-    name,
-    ['education-1'] : 'bachelor',
-    [`education-${number}`] : 'bachelor'
+const person3 = {
+        age: 24,
+        name,
+        ['education-1']: 'bachelor',
+        [`education-${number}`]: 'bachelor'
 }
 // ==> {age: 24, name: 'mina', education-1: 'bachelor', education-10: 'bachelor'}
 
 const school = {
-    name: 'Hafez',
-    getName:function(){
-        return this.name
-    }
+        name: 'Hafez',
+        getName: function () {
+                return this.name
+        }
 }
 // school.name
 // 'Hafez
 
 const school1 = {
-    name: 'Hafez',
-    getName(){
-        return this.name
-    }
+        name: 'Hafez',
+        getName() {
+                return this.name
+        }
 }
 // school.getName();
 // 'Hafez
 const school2 = {
-    name: 'Hafez',
-    getName(){
-        return this.name
-    },
-    setName(name){
-        this.name = name
-    }
+        name: 'Hafez',
+        getName() {
+                return this.name
+        },
+        setName(name) {
+                this.name = name
+        }
 }
 
 // school2.getName()

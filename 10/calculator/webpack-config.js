137bf98004b8d5c3b15d037ceb9a045eb4a93ab9
minashaker77 <minashk1998@gmail.com>
2023-05-10 19:06:22 +0330

    use webpack installation

---
+++ 10/calculator/webpack-config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};

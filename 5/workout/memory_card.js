234c00eea16a1723c2c9749792f72ec795b3ccd2
Mina Shaker <minashk1998@gmail.com>
2023-05-20 21:19:09 +0330

    chech if card value is not null send it to db

--- 5/workout/memory_card.js
+++ 5/workout/memory_card.js
@@ -5,8 +5,8 @@ var desc = document.getElementById('descID');
 
 const cardsUrl = 'http://localhost:3000/cards'
 
-function fetchCardContent(address = cardsUrl) {
-    return fetch(address)
+async function fetchCardContent(address = cardsUrl) {
+    return await fetch(address)
         .then(response => response.json())
         .then(({ data }) => {
             return data
@@ -15,25 +15,28 @@ function fetchCardContent(address = cardsUrl) {
 
 
 let onClickAdd = (event) => {
+
     event.preventDefault()
     console.log(title.value)
     console.log(desc.value)
     // title.value = ""
     // desc.value = ""
-    const saveDataToDB = {
-        "title": title.value,
-        "desc": desc.value,
-    };
-    fetch(cardsUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(saveDataToDB)
-    })
-        .then(response => response.json())
-        .then(() => {
-            title.value = ""
-            desc.value = ""
+    if (title.value !== "" && desc.value !== "") {
+        const saveDataToDB = {
+            "title": title.value,
+            "desc": desc.value,
+        };
+        fetch(cardsUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(saveDataToDB)
         })
+            .then(response => response.json())
+            .then(() => {
+                title.value = ""
+                desc.value = ""
+            })
+    }
 }
